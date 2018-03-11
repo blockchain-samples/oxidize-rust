f4911afa529241ba2917e4cd49dae09dfd98c555
theochupp <tclchiam@gmail.com>
2018-03-11 16:34:02 -0400

    Added a function that returns the genesis block (still needs public key hash!!)

--- src/chain/entity/block.rs
+++ src/chain/entity/block.rs
@@ -1,5 +1,8 @@
 use chain::entity::header::Header;
+use chain::entity::tx::Output;
 use chain::entity::tx::Transaction;
+use hash::EMPTY_HASH;
+use hash::hash_from_string;
 
 #[derive(Debug)]
 pub struct Block {
@@ -16,5 +19,27 @@ impl Block {
             header,
             transactions,
         }
+    }
+}
+
+pub fn genesis_block() -> Block {
+    Block {
+        header: Header {
+            index: 0,
+            previous_hash: EMPTY_HASH,
+            timestamp: 1516146240,
+            transactions_hash: hash_from_string("85fca3e06fe7196148c3f6beae1aeb7dc8b9308ce26bbd0f32bda91a60d63bbc").unwrap(),
+            nonce: 84167,
+            hash: hash_from_string("00007bd9a44c3fea74388c483c3fc2fc8ac593c3da5566fbc1427cbf023e4ed9").unwrap(),
+            difficulty: 4,
+        },
+        transactions: vec![
+            Transaction {
+                id: hash_from_string("d81f935f0c45cd0df0ccf073ae0e33432dd14cd925262a51a5bb43a77f433862").unwrap(),
+                inputs: vec![],
+                outputs: vec![Output { index: 0, value: 10 }],
+                secret: hash_from_string("39f39efae3884e28f5a5c4a62dd994e2943ac9cc7f9684070dfd4add7353722f").unwrap(),
+            }
+        ],
     }
 }
